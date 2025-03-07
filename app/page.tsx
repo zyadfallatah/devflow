2b7ed59ceda4c608ae4f76dde02177a421e78bc7
zyadfallatah <zyadfallatah@gmail.com>
2025-03-07 04:14:03 +0300

    Tailwindcss theme setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hello from "@/components/Hello";
 const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-violet-500">Hello From Server</h1>
+      <h1 className="h1-bold">Hello From Server</h1>
       <Hello />
     </>
   );
