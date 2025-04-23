ba21fca4a5ddd4259f3aafdbc9ddf1f07b9e107e
zyadfallatahUQU <zyadfallatah@gmail.com>
2025-04-23 10:06:48 +0300

    intial mongoDb & mongoose setup

---
+++ lib/mongoose.ts
@@ -0,0 +1,47 @@
+import { changeWith } from "@mdxeditor/editor";
+import mongoose, { Mongoose } from "mongoose";
+
+const mongooseURI = process.env.MONGODB_URI as string;
+
+if (!mongooseURI) {
+  throw new Error("MONGODB URI is not defined");
+}
+
+interface MongooseCahed {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCahed;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(mongooseURI, {
+        dbName: "devFlow",
+      })
+      .then((result) => {
+        console.log("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        console.error("Failed to connect to database\n", error);
+        return error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.promise;
+};
