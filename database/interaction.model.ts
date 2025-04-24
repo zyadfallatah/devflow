9e144191fc0878d454ebac05bd02036d9f214522
zyadfallatah <zyadfallatah@gmail.com>
2025-04-24 20:02:40 +0300

    interaction model added

---
+++ database/interaction.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: "question" | "answer";
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: { type: String, required: true },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
