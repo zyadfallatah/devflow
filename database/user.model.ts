c900d61316d683aaca2d3064e060d7a9d4de691e
zyadfallatah <zyadfallatah@gmail.com>
2025-04-24 17:16:55 +0300

    User model added

---
+++ database/user.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema } from "mongoose";
+
+interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portofolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portofolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
