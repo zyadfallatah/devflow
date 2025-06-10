523889f9a70b7a5c0040bf4209f116d051636863
zyadfallatah <zyadfallatah@gmail.com>
2025-06-10 07:44:25 +0300

    edit logger logic

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,20 +1,19 @@
 import pino from "pino";
 
-const isEdge = process.env.NEXT_RUNTIME === "edge";
 const isProduction = process.env.NEXT_RUNTIME === "production";
 
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  transport:
-    // isEdge && !isProduction
-    {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        ignore: "pid,hostname",
-        translateTime: "SYS:standard",
+  transport: isProduction
+    ? { target: "" }
+    : {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          ignore: "pid,hostname",
+          translateTime: "SYS:standard",
+        },
       },
-    },
   formatters: {
     level: (label) => ({ level: label.toUpperCase() }),
   },
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["pino"],
+  serverExternalPackages: ["pino", "pino-pretty"],
   transpilePackages: ["next-mdx-remote"],
   eslint: {
     ignoreDuringBuilds: true,
